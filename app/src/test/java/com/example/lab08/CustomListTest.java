2adac145654fed732bc987c59b9a92323f1d4bfa
Muhammad-Zohair04 <muhammadzohair2004@gmail.com>
2026-03-19 11:07:28 +0500

    test case failing (2)

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,6 +1,9 @@
 package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
 public class CustomListTest {
     @Test
     public void testHasCity() {
@@ -9,6 +12,17 @@ public class CustomListTest {
         list.addCity(calgary);
         // This will fail initially because hasCity() doesn'texist
         assertTrue(list.hasCity(calgary));
+    }
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+
+        City city = new City("Edmonton", "Alberta");
+        list.addCity(city);
+
+        list.deleteCity(city);
+
+        assertFalse(list.hasCity(city));
     }
 
 }
